4c8b6e1456709a9cdba8bd23ea34debdc4814d8f
Adam Wootton <ajwootto@uwaterloo.ca>
2014-07-21 12:43:13 -0400

    hacked together for lesia

--- server/server.py
+++ server/server.py
@@ -1,24 +1,29 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask import request
 from werkzeug.contrib.cache import SimpleCache
 import time
 import threading
-import bitalinoThread
+#import bitalinoThread
 import classifier
 
 USER_STATES = classifier.USER_STATES
 cache = SimpleCache()
 app = Flask(__name__)
 
+def user_state(bpm=100, stress=0):
+	return {'bpm': bpm, 'stress': stress}
+
+machines_stopped = {'drill': 'false', 'band': 'false', 'lathe': 'false', 'mill': 'false', 'saw': 'false', 'grinder': 'false'}
+users = {'adam': user_state(),'nick': user_state(),'lesia': user_state(), 'mayank': user_state(),'tom': user_state(),'vic': user_state()}
 
 def initialize_cache():
-	cache.set("machine-is-stopped", 'false') #represents the machine state in the real world
-	cache.set("user-state", USER_STATES["normal"])
+	cache.set("machine-stops", machines_stopped) #represents the machine state in the real world
+	cache.set("user-states", users)
 
 initialize_cache()
 
-bitalino_thread = bitalinoThread.Bitalino_Thread()
-bitalino_thread.start()
+#bitalino_thread = bitalinoThread.Bitalino_Thread()
+#bitalino_thread.start()
 
 #ROUTING
 @app.route("/", methods=['GET', 'POST'])
@@ -31,11 +36,20 @@ def index_page():
 @app.route("/stop", methods=['GET', 'POST'])
 def stop_system():
 	if request.method == 'GET':
-		return cache.get("machine-is-stopped")
+		return cache.get("machine-stops")
 	else:
-		cache.set("machine-is-stopped", request.form['stopped'])
-		return cache.get("machine-is-stopped")
+		cache.set("machine-stops", request.form['stopped'])
+		return cache.get("machine-stops")
 
+@app.route("/users", methods=['GET', 'POST'])
+def user():
+	if request.method == 'GET':
+		return jsonify(cache.get("user-states"))
+	else:
+		states = cache.get("user-states")
+		states[request.form['name']] = user_state(request.form['bpm'], request.form['stress'])
+		cache.set("user-states", states)
+		return jsonify(cache.get("user-states"))
 
 if __name__ == "__main__":
 	app.run(debug=True)
